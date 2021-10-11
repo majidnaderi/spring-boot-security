05db4e4d2af9adc26daae738634a92a7444ca7dd
Majid Naderi <majidnaderi51@gmail.com>
2021-10-11 20:07:50 +0330

    ROLES & PERMISSIONS USING ENUMS

--- src/main/java/ir/learning/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/ir/learning/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -1,23 +1,63 @@
 package ir.learning.springbootsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/","index","/css/*","/js/*")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
-//    }
-//}
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import static ir.learning.springbootsecurity.security.ApplicationUserRole.*;
+
+import static ir.learning.springbootsecurity.security.ApplicationUserRole.*;
+
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails majid  = User.builder()
+                .username("majid")
+                .password(passwordEncoder.encode("123"))
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails vahid  = User.builder()
+                .username("vahid")
+                .password(passwordEncoder.encode("123"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                majid,
+                vahid
+        );
+
+    }
+}
---
+++ src/main/java/ir/learning/springbootsecurity/security/ApplicationUserRole.java
@@ -0,0 +1,18 @@
+package ir.learning.springbootsecurity.security;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+import static ir.learning.springbootsecurity.security.ApplicationUserPermission.*;
+
+public enum ApplicationUserRole {
+    STUDENT(Sets.newHashSet()),
+    ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE));
+    private final Set<ApplicationUserPermission> permissions;
+
+    ApplicationUserRole(Set<ApplicationUserPermission> permissions) {
+        this.permissions = permissions;
+    }
+
+}
